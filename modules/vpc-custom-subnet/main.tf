c13244581275b4460129f74e5d9edad0f890496d
Jonas Crescencio <jonas.crescencio@jonas.stone.local>
2022-06-27 10:00:17 -0300

    Feat: Add vpn custom subnet module

--- modules/vpc-custom-subnet/main.tf
+++ modules/vpc-custom-subnet/main.tf
@@ -2,23 +2,50 @@
 # Custom subnet(s)
 ################################################################################
 
+locals {
+  private_custom_subnets = flatten([
+    for block in var.private_custom_blocks : [
+      for subnet in block.subnets : {
+        subnet = subnet
+        azs = block.azs
+        subnet_suffix = block.subnet_suffix
+        tags = block.tags
+      }
+    ]
+  ])
+}
+
 resource "aws_subnet" "custom" {
-  count = var.create_vpc && length(var.custom_subnets) > 0 ? length(var.custom_subnets) : 0
+  count = var.create_vpc && length(local.private_custom_subnets) > 0 ? length(local.private_custom_subnets) : 0
 
   vpc_id               = var.vpc_id
-  cidr_block           = var.custom_subnets[count.index]["cidr_block"]
+  cidr_block           = local.private_custom_subnets[count.index].subnet
 
-  availability_zone    = length(regexall("^[a-z]{2}-", var.custom_subnets[count.index]["az"])) > 0 ? var.custom_subnets[count.index]["az"] : null
-  availability_zone_id = length(regexall("^[a-z]{2}-", var.custom_subnets[count.index]["az"])) == 0 ? var.custom_subnets[count.index]["az"] : null
-
+  availability_zone    = length(regexall("^[a-z]{2}-", element(local.private_custom_subnets[count.index].azs, count.index))) > 0 ? element(local.private_custom_subnets[count.index].azs, count.index) : null
+  availability_zone_id = length(regexall("^[a-z]{2}-", element(local.private_custom_subnets[count.index].azs, count.index))) == 0 ? element(local.private_custom_subnets[count.index].azs, count.index) : null
+  assign_ipv6_address_on_creation =  var.private_custom_subnet_assign_ipv6_address_on_creation == null ? var.assign_ipv6_address_on_creation : var.private_custom_subnet_assign_ipv6_address_on_creation
   tags = merge(
     {
       "Name" = format(
-        "${var.name}-${var.custom_subnets[count.index]["subnet_suffix"]}-%s",
-        var.custom_subnets[count.index]["az"]
+        "${var.name}-${local.private_custom_subnets[count.index].subnet_suffix}-%s",
+        element(local.private_custom_subnets[count.index].azs, count.index),
       )
     },
     var.tags,
-    var.custom_subnets[count.index]["tags"],
+    local.private_custom_subnets[count.index].tags,
+  )
+}
+
+################################################################################
+# Route table association
+################################################################################
+
+resource "aws_route_table_association" "custom_private" {
+  count = var.create_vpc && length(local.private_custom_subnets) > 0 ? length(local.private_custom_subnets) : 0
+
+  subnet_id = element(aws_subnet.custom[*].id, count.index)
+  route_table_id = element(
+    var.private_route_table_ids,
+    var.single_nat_gateway ? 0 : count.index,
   )
 }
--- modules/vpc-custom-subnet/variables.tf
+++ modules/vpc-custom-subnet/variables.tf
@@ -18,14 +18,38 @@ variable "tags" {
   description = "Define general tags"
 }
 
-variable "custom_subnets" {
-  type        = list(any)
-  description = "Define custom subnets"
-  default     = []
+variable "private_custom_blocks" {
+  type        = map(any)
+  description = "Define custom blocks"
+  default     = {}
+}
+
+variable "private_route_table_ids" {
+  type        = list(string)
+  description = "Define private route table ids"
+  default = []
 }
 
 variable "vpc_id" {
   description = "The ID of the VPC in which the endpoint will be used"
   type        = string
   default     = null
+}
+
+variable "private_custom_subnet_assign_ipv6_address_on_creation" {
+  description = "Assign IPv6 address on private custom subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch"
+  type        = bool
+  default     = null
+}
+
+variable "assign_ipv6_address_on_creation" {
+  description = "Assign IPv6 address on subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch"
+  type        = bool
+  default     = false
+}
+
+variable "single_nat_gateway" {
+  description = "Should be true if you want to provision a single shared NAT Gateway across all of your private networks"
+  type        = bool
+  default     = false
 }
